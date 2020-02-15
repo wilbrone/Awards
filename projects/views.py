8dd50becc5ebba1cc09e516eb137948666316c07
wilbrone <wilbroneokoth@gmail.com>
2020-02-15 15:57:30 +0300

    add search functionality and detail views for projects

--- projects/views.py
+++ projects/views.py
@@ -121,4 +121,66 @@ def posts(request):
 
     return render(request, 'post.html', {'form': form}, {'users':users})
 
-    
+
+@login_required(login_url='login')
+def single_post(request, post):
+    # image = get_object_or_404(Project, pk=id)
+    post = Project.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rating.objects.filter(post=post)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            print(score)
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    params = {
+        'post': post,
+        'rating_form': form,
+        'rating_status': rating_status
+
+    }
+    return render(request, 'singlepost.html', params)
+
+    # return render(request, 'single_post.html', {'image':image})
+
+@login_required(login_url='login')
+def search_project(request):
+    if request.method == 'GET':
+        title = request.GET.get("title")
+        details = Project.objects.filter(title__icontains=title).all()
+        message = f'name'
+        params = {
+            'details': details,
+            'message': message
+        }
+        return render(request, 'search.html', params)
+    else:
+        message = "You haven't searched for any project"
+    return render(request, 'search.html', {'message': message})
