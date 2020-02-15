088c96db1830aa33ee5af61af4cab1a82f6e7e25
wilbrone <wilbroneokoth@gmail.com>
2020-02-15 15:06:49 +0300

    add more view functions

--- projects/views.py
+++ projects/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from .serializer import ProjectSerializer, ProfileSerializer
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status 
 
 from .forms import *
