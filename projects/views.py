0a486ff83b0e352cbbdf8930db5a83ed82e80aa5
wilbrone <wilbroneokoth@gmail.com>
2020-02-15 14:46:26 +0300

    add class def in view

--- projects/views.py
+++ projects/views.py
@@ -62,3 +62,38 @@ def profile(request):
 def index(request):
 
     return render(request, 'prjts/index.html')
+
+
+
+class ProjectList(APIView):
+
+    # handling a retrieval request
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+    # handling a post request
+    def post(self,request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class ProfileList(APIView):
+
+    # handling a retrieval request
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+
+        
